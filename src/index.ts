1ab69938272116b6ee1ea5c4a6a6b548cd6f8575
DacDoan-Grik <18520551@gm.uit.edu.vn>
2022-03-04 16:58:43 +0700

    Setup project

---
+++ src/index.ts
@@ -0,0 +1,34 @@
+import dotenv from "dotenv";
+import express from 'express';
+import mongoose from "mongoose";
+import cors from "cors";
+import morgan from "morgan";
+
+dotenv.config();
+
+const app = express();
+
+//Midleware
+
+app.use(cors());
+app.use(morgan('dev'));
+app.use(express.json());    //Chose datatype for express
+app.use(express.urlencoded())  //Accecpt html form
+
+//Database
+const URI = process.env.MONGO_URL;
+mongoose.connect(URI,{
+    autoIndex: false,
+}, (err)=> {
+    if(err) throw err;
+    console.log("Connection successfully!");
+});
+
+//Routes
+app.get('/', (req:any,res: any)=> { res.json({ok:"ok"})})
+
+//Start server listening
+const port = process.env.PORT || 5000;
+app.listen(port, ()=> {
+    console.log(`Listening on port ${port}`)
+})
